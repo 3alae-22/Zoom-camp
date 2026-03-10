f271c18956a32ab0e964f08b89bc0403785ad7d3
3alae-22 <alaeaghoutane32@gmail.com>
2026-03-10 11:15:23 +0000

    pipeline

---
+++ pipeline/pipeline.py
@@ -0,0 +1,9 @@
+import sys
+import pandas as pd
+print(sys.argv)
+month=int(sys.argv[1])
+df=pd.DataFrame({"A":[1,2],"B":[3,4]})
+df['month']=month
+print(df.head())
+df.to_parquet(f"output_{month}.parquet")
+print("Hello pipeline, month=",month)
